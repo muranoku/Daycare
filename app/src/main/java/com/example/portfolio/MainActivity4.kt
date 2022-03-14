d6e136f4270913429cc67381f94515e35583b5b5
garagara <ayuyeaun@gmail.com>
2022-03-15 01:38:12 +0900

    Add:クイズ機能追加

---
+++ app/src/main/java/com/example/portfolio/MainActivity4.kt
@@ -0,0 +1,16 @@
+package com.example.portfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+
+        val tvCount : TextView = findViewById(R.id.tvCount)
+        val foodQ : TextView = findViewById(R.id.foodQ)
+        //val
+    }
+}
